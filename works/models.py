756855cff1241d06d0633495ccf1618a3a2e449f
Kenno-Warise <wariken0523@gmail.com>
2022-10-22 18:11:39 +0900

    データベースに照合

--- works/models.py
+++ works/models.py
@@ -1,8 +1,92 @@
-from django.contrib.auth.models import AbstractBaseUser
 from django.db import models
+from django.core.validators import MinLengthValidator
+from django.contrib.auth.validators import UnicodeUsernameValidator
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser, PermissionsMixin
+
+
+class UserManager(BaseUserManager):
+    """カスタムユーザーマネージャー"""
+    def create_user(self, user_no, password=None):
+        if not user_no:
+            raise ValueError('社員番号を入力してください。')
+
+        # user = self.model(
+        #     email=self.normalize_email(email),
+        #)     
+        user = self.model(user_no=user_no)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_staffuser(self, user_no, password):
+        user = self.create_user(
+            user_no,
+            password=password,
+        )
+        user.staff = True
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, user_no, password):
+        user = self.create_user(
+            user_no,
+            password=password,
+        )
+        user.staff = True
+        user.admin = True
+        user.save(using=self._db)
+        return user
 
 
 class User(AbstractBaseUser):
-    user_no = models.CharField(verbose_name="社員番号", max_length=4)
+    """カスタムユーザーモデル"""
+    user_no = models.CharField(
+        verbose_name='社員番号',
+        max_length=4,
+        help_text="4桁で番号を入力。",
+        unique=True,
+        error_messages = {
+            'unique': '既に使用されている番号です',
+        },
+        validators=[MinLengthValidator(4)], # 最低の文字数を検証・設定
+    )
+    username_validators = UnicodeUsernameValidator() # ユーザーネームに関する検証
+    username = models.CharField(
+            verbose_name='ユーザー名',
+            max_length=150,
+            help_text="150文字以下の文字や数字、一部の記号で入力してください。",
+            validators=[username_validators],
+    )
+    active = models.BooleanField(default=True)
+    staff = models.BooleanField(default=False) 
+    admin = models.BooleanField(default=False) 
+   
+    USERNAME_FIELD = 'user_no'
+
+    # User.objectsを呼び出すための変数
+    objects = UserManager()
+
+    def __str__(self):             
+        return self.user_no
+
+
+    def has_perm(self, perm, obj=None):
+        return self.admin
+
+    def has_module_perms(self, app_label):
+        return self.admin
+
+    @property
+    def is_staff(self):
+        return self.staff
+
+    @property
+    def is_admin(self):
+        return self.admin
+
+    @property
+    def is_active(self):
+        return self.active
+    
 
 # Create your models here.
