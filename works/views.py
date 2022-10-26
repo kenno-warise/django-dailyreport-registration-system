b7b4a85eae4820cc2f40b8a0232bc00aca073106
Kenno-Warise <wariken0523@gmail.com>
2022-10-26 13:11:48 +0900

    社員側の月別リストをコピーして再利用し、社員の詳細を表示するために社員IDから社員の月別リストを表示

--- works/views.py
+++ works/views.py
@@ -191,8 +191,42 @@ def user_list(request):
     return render(request, 'works/user_list.html', context)
 
 
-def user_result(request, id):
+def user_result(request, user_id):
     """日報登録＆月別リスト"""
-    return render(request, 'works/user_result.html')
+    form = EveryMonthForm()
+    work = get_object_or_404(Work, user_id=user_id, date=timezone.now().date().strftime("%Y-%m-%d"))
+    # work = Work.objects.filter(user_id=request.user.id, date=timezone.now().date().strftime("%Y-%m-%d"))
+    modal_form = WorkForm(instance=work)
+    if request.user.id:
+        # lastday変数に月末日の生成
+        _, lastday = calendar.monthrange(timezone.now().year, timezone.now().month)
+        # 過去から今月末までのデータを取得且つ、今月のみのデータを取得（社員IDを持っている人のみ）
+        user_works = Work.objects.order_by("date").filter(
+                date__lte=timezone.now().date().replace(day=lastday),
+                date__year=form.dates[0][0].split('/')[0],
+                date__month=form.dates[0][0].split('/')[1],
+                user_id=request.user.id
+        )
+    else:
+        user_works = None
+    if request.method == "POST":
+        post_data = request.POST
+        # 新たにPOSTされたデータを使用して更新用データを取得
+        edit_work = get_object_or_404(
+                Work,
+                user_id=post_data['user_id'],
+                date=post_data['date']
+        )
+        modal_form = WorkForm(request.POST, instance=edit_work)
+        if modal_form.is_valid():
+            modal_form.save()
+            return redirect('works:user-result', user_id)
+    context = {
+            'user_works': user_works,
+            'form': form,
+            'modal_form': modal_form,
+            'work': work,
+    }
+    return render(request, 'works/user_result.html', context)
 
 # Create your views here.
